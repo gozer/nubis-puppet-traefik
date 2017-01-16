f538ee743235263adbc5c7f5cf8e52e7e8de5039
Philippe M. Chiasson <gozer@ectoplasm.org>
2017-01-16 10:24:44 -0500

    install service

--- manifests/init.pp
+++ manifests/init.pp
@@ -44,9 +44,9 @@
 #
 
 class nubis_traefik($version = '1.1.2') {
-  $traefik_url = "https://github.com/containous/traefik/releases/download/v${traefik_version}/traefik_linux-amd64"
+  $traefik_url = "https://github.com/containous/traefik/releases/download/v${version}/traefik_linux-amd64"
 
-  notice ("Grabbing traefik ${traefik_version}")
+  notice ("Grabbing traefik ${version}")
 
   staging::file { '/usr/local/bin/traefik':
     source => $traefik_url,
@@ -62,5 +62,43 @@ class nubis_traefik($version = '1.1.2') {
     owner  => root,
     group  => root,
     mode   => '0640',
+  }
+
+  upstart::job { 'traefik':
+    description    => 'Traefik Load Balancer',
+    service_ensure => 'stopped',
+    require        => [
+      Staging::File['/usr/local/bin/traefik'],
+    ],
+    # Never give up
+    respawn        => true,
+    respawn_limit  => 'unlimited',
+    start_on       => '(local-filesystems and net-device-up IFACE!=lo)',
+    env            => {
+      'SLEEP_TIME' => 1,
+      'GOMAXPROCS' => 2,
+    },
+    user           => 'root',
+    group          => 'root',
+    script         => '
+  if [ -r /etc/profile.d/proxy.sh ]; then
+    echo "Loading Proxy settings"
+    . /etc/profile.d/proxy.sh
+  fi
+
+  exec /usr/local/bin/traefik --web.readonly=true --loglevel=INFO
+',
+    post_stop      => '
+goal=$(initctl status $UPSTART_JOB | awk \'{print $2}\' | cut -d \'/\' -f 1)
+if [ $goal != "stop" ]; then
+    echo "Backoff for $SLEEP_TIME seconds"
+    sleep $SLEEP_TIME
+    NEW_SLEEP_TIME=`expr 2 \* $SLEEP_TIME`
+    if [ $NEW_SLEEP_TIME -ge 60 ]; then
+        NEW_SLEEP_TIME=60
+    fi
+    initctl set-env SLEEP_TIME=$NEW_SLEEP_TIME
+fi
+',
   }
 }
